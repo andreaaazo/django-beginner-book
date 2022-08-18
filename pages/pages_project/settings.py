8e6e7b3a6841570da75e7f75a8c273dc549f762d
Andrea Zorzi <107885651+andreaaazo@users.noreply.github.com>
2022-08-18 03:20:32 +0200

    Removed secret key

--- pages/pages_project/settings.py
+++ pages/pages_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ca%0&^ylgqq+7ze4go0-ftyn=py=^3bd($&+*b_vk34k(*a8e2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
